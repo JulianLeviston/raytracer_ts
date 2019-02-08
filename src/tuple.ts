1b49a267a189dff19f3197180098896d53cdf7aa
Julian Leviston <julian@leviston.net>
2019-02-08 14:19:51 +1100

    Initial commit

---
+++ src/tuple.ts
@@ -0,0 +1,102 @@
+import { zipWith } from 'lodash'
+
+class Tuple {
+  /**
+   * A tuple is a set of floats
+   * For our purposes, we allow accesors
+   * so they can act like points or colours
+   * by convention, and to allow maths on them
+   */
+  private value: number[]
+  constructor(...args) {
+    this.value = args
+  }
+  get values(): number[] { return this.value }
+  get x(): number { return this.value[0] }
+  get y(): number { return this.value[1] }
+  get z(): number { return this.value[2] }
+  get w(): number { return this.value[3] }
+}
+
+function tuple(...args): Tuple {
+  return new Tuple(...args)
+}
+
+/**
+ * constructor function for creating points.
+ * note that a point is simply a tuple whose w
+ * component is 1
+ * 
+ * @param x the x (first) component of the point 
+ * @param y the y (second) component of the point 
+ * @param z the z (third) component of the point
+ */
+function point(x: number, y: number, z: number): Tuple {
+  return tuple(x, y, z, 1)
+}
+
+/**
+ * constructor function for creating vectors.
+ * note that a point is simply a tuple whose w
+ * component is 0
+ * 
+ * @param x the x (first) component of the vector 
+ * @param y the y (second) component of the vector 
+ * @param z the z (third) component of the vector
+ */
+function vector(x: number, y: number, z: number): Tuple {
+  return tuple(x, y, z, 0)
+}
+
+/**
+ * Test whether a tuple is a point
+ * @param tuple the tuple to test
+ */
+function isPoint(tuple: Tuple): Boolean {
+  return tuple.w == 1
+}
+
+/**
+ * Test whether a tuple is a vector
+ * @param tuple the tuple to test
+ */
+function isVector(tuple: Tuple): Boolean {
+  return tuple.w == 0
+}
+
+/**
+ * Create a new tuple whose components are the
+ * sum of the components of two passed in tuples
+ * @param t1 first tuple to add
+ * @param t2 second tuple to add
+ */
+function add(t1: Tuple, t2: Tuple): Tuple {
+  const op = (x: number, y: number): number => x + y
+  return applyBinOp(op, t1, t2)
+}
+
+/**
+ * Create a new tuple whose components are the
+ * sum of the components of two passed in tuples
+ * @param t1 first tuple to add
+ * @param t2 second tuple to add
+ */
+function sub(t1: Tuple, t2: Tuple): Tuple {
+  const op = (x: number, y: number): number => x - y
+  return applyBinOp(op, t1, t2)
+}
+
+/**
+ * Create a new tuple whose components are the
+ * result of applying the `op` function to each of the
+ * components of the passed in tuples, pair-wise
+ * @param op binary operation (ie arity of 2) on numbers to be applied to each tuple component pair
+ * @param t1 first tuple to apply `op` to
+ * @param t2 second tuple to apply `op` to
+ */
+function applyBinOp(op: (x: number, y: number) => number, t1: Tuple, t2: Tuple): Tuple {
+  const appliedTupleValues: number[] = zipWith(t1.values, t2.values, op)
+  return tuple(...appliedTupleValues)
+}
+
+export { tuple, point, vector, isPoint, isVector, add, sub }
---
+++ test/tuples-test.ts
@@ -0,0 +1,55 @@
+import test from 'ava';
+import {
+  tuple,
+  point,
+  vector,
+  isPoint,
+  isVector,
+  add,
+  sub,
+ } from '../src/tuple'
+
+test('Tuple creation and field access of tuple 4 3 2 1', t => {
+  const myTuple = tuple(4, 3, 2, 1)
+  t.is(myTuple.x, 4, 'has x=4')
+  t.is(myTuple.y, 3, 'has y=3')
+  t.is(myTuple.z, 2, 'has z=2')
+  t.is(myTuple.w, 1, 'has w=1')
+})
+
+test('Point creation', t => {
+  const myTuple = tuple(4, 3, 2, 1)
+  const myPoint = point(4, 3, 2)
+  t.deepEqual(myTuple, myPoint, 'A tuple with w=1.0 is a point')
+  t.is(myPoint.w, 1, 'A point should have w=1.0')
+  const aTupleWithWOf1 = tuple(1, 2, 3, 1)
+  t.is(isPoint(aTupleWithWOf1), true, 'A tuple with w=1.0 tests as a point')
+  t.is(isVector(aTupleWithWOf1), false, "A tuple with w=1.0 doesn't test as a vector")
+})
+
+test('Vector creation', t => {
+  t.deepEqual(tuple(4, 3, 2, 0), vector(4, 3, 2), 'A tuple with w=0 is a vector')
+  t.is(vector(1, 2, 3).w, 0, 'A vector should have w=0')
+  const aTupleWithWOf0 = tuple(1, 2, 3, 0)
+  t.is(isVector(aTupleWithWOf0), true, 'A tuple with w=0 tests as a vector')
+  t.is(isPoint(aTupleWithWOf0), false, "A tuple with w=0 doesn't test as a point")
+})
+
+test('Addition', t => {
+  const a1 = tuple(3, -2, 5, 1)
+  const a2 = tuple(-2, 3, 1, 0)
+  const resultTuple = tuple(1, 1, 6, 1)
+  t.deepEqual(add(a1, a2), resultTuple, 'Adding two tuples works correctly')
+})
+
+test('Subtraction and Negation', t => {
+  const p1 = point(3, 2, 1)
+  const p2 = point(5, 6, 7)
+  const expected1 = vector(-2, -4, -6)
+  t.deepEqual(sub(p1, p2), expected1, 'Subtracting one point from another works correctly')
+  const p = point(3, 2, 1)
+  const v = vector(5, 6, 7)
+  const expected2 = point(-2, -4, -6)
+  t.deepEqual(sub(p, v), expected2, 'Subtracting a vector from a point works correctly')
+})
+
